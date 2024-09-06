2733902f6c7fd1a973986f52f7a17e264cb33ce8
NEDDL <mustafasaitonal@outlook.com>
2024-09-06 17:43:18 +0200

    add debug

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -6,13 +6,21 @@ module.exports = ({ strapi }) => {
     if (event.action === "beforeFindMany" || event.action === "beforeFindOne") {
       const deepPopulate = event.params?.deeppopulate;
       const defaultDepth =
-        strapi.plugin("strapi-plugin-populate-deep")?.config("defaultDepth") ||
-        5;
+        strapi.plugin("custom-deep-populate")?.config("defaultDepth") || 5;
+
+      console.log("Custom Plugin: Deep populate param:", deepPopulate); // Debugging
 
       if (deepPopulate && deepPopulate[0] === "deep") {
         const depth = deepPopulate[1] ?? defaultDepth;
+        console.log(
+          `Custom Plugin: Applying deep population with depth: ${depth}`
+        ); // Debugging
         const modelObject = getFullPopulateObject(event.model.uid, depth, []);
         event.params.populate = modelObject.populate;
+        console.log(
+          "Custom Plugin: Final populate object:",
+          event.params.populate
+        ); // Debugging
       }
     }
   });
